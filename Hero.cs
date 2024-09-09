fbfd9daa0af6572fbea4df38db09ad1e539fd10f
ScrubbyAlien <samuel.eskilson@gmail.com>
2024-09-09 16:31:58 +0200

    created hero class and new game

---
+++ Hero.cs
@@ -0,0 +1,9 @@
+﻿namespace TextAdventure;
+
+public class Hero
+{
+    public string Name = "";
+    public int Health = 100;
+    public List<string> Items = new List<string>();
+    public string Location = "newgame";
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace TextAdventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Welcome to Text Adventure!!!!!!!!!!!");
+
+            string name = "";
+            while (name == "")
+            {
+                name = Ask("What is your name adventurer?\n");
+
+                if (!AskYesOrNo($"Is your name {name}?"))
+                {
+                    name = "";
+                }
+            }*/
+
+            Hero hero = new();
+            NewGame(hero);
+            while (hero.Location != "quit")
+            {
+                break;
+            }
+
+        }
+
+        static void NewGame(Hero hero)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Text Adventure!!!!!!!!!!!");
+
+            string name = "";
+            while (name == "")
+            {
+                name = Ask("What is your name adventurer?\n");
+
+                if (!AskYesOrNo($"Is your name {name}?"))
+                {
+                    name = "";
+                }
+            }
+
+            hero.Name = name;
+            hero.Location = "tableroom";
+
+        }
+        
+        static string Ask(string question)
+        {
+            string response = "";
+            while (response == "")
+            {
+                Console.Write(question);
+                response = Console.ReadLine().Trim();
+            }
+
+            return response;
+        }
+
+        static bool AskYesOrNo(string question)
+        {
+            while (true)
+            {
+                string response = Ask(question + " (y/n) ").ToLower();
+                if (response == "y") return true;
+                if (response == "n") return false;
+            }
+        }
+
+    }
+}
